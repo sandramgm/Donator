b3538ca80361f2afd138377756952da8c3f1b2a3
Sandra <sandramgm@yahoo.com>
2013-03-24 19:38:00 +0100

    Video behaviour
    - Pause onblur
    - Finish notice
    - Disable seeking

--- wp-content/themes/modernize_v2-23/donator/js/scripts-default.js
+++ wp-content/themes/modernize_v2-23/donator/js/scripts-default.js
@@ -1,72 +1,43 @@
-/*
-jQuery(document).ready(function($) {
-	$(".Donator_LightboxLink").fancybox({
-		maxWidth: document.width,
-		minWidth: document.width - 100,
-		maxHeight: document.height - 100,
-	       width: 700,
-	       height: 500,
-		scrollOutside:true
-	});
-});*/
-/*
-var lightbox = new Donator_Object_Lightbox;
-lightbox.start();*/
-
-/*
-(function($) {  
-	$('.Donator_LightboxLink').click(function() {
-		var campaign = this.attr('data-video');
-		alert(campaign);
-	}); 
-
-	var videoObj = new Donator_Object_Video;
-	
-})(jQuery);*/
-
-//jQuery(document).ready(function($) {
-	// Inside of this function, $() will work as an alias for jQuery()
-	// and other libraries also using $ will not be accessible under this shortcut
-
-var videoLink = jQuery('.Donator_LightboxLink');
 var isActive, videoObj;
 
 videoLink.click(function() {
-  var campaign = jQuery(this).attr('data-campaign');
+    var campaign = jQuery(this).attr('data-campaign');
 		
-  /** Create new video object and open a lightbox */
-  videoObj = createVideo(campaign, videoLink);
-  videoObj.showLightbox();
-  
-/*  isActive = true;
-  console.log(window.isActive? 'active OK' : 'inactive NO') ;*/
-  
-  //jQuery('#other-pause').onclick(videoObj._settings.player.pause());
-  
+    /** 
+     * Create new video object and open a lightbox 
+     * */
+    videoObj = createVideo(campaign);
+ 
+    /**
+     * Display lightbox with campaign details (video, info, ONGs)
+     * */
+    videoObj.showLightbox();
 });
 
 /**
 * This functions are used to defermine if the browser tab is active or inactive
 * */
 window.addEventListener('focus', function() {
-	isActive = true;
+    if ((videoObj && videoObj._status != 'none') && videoObj._settings.player.playing == true) {
+    	videoObj._status = 'active';
+    }
+	isActive = 'active';
+    console.log(window.isActive);
 });
 
 window.addEventListener('blur', function() {
-	isActive = false;
-	videoObj._settings.player.pause();
-	//jQuery(".flowplayer").pause();
+    if ((videoObj && videoObj._status != 'none') && videoObj._settings.player.playing == true) {
+    	videoObj._status = 'inactive';
+        videoObj._settings.player.pause(function() { console.log('pause'); });
+    }
+    isActive = 'inactive';
+    console.log(window.isActive);
 });
-
-setInterval(function () { console.log(window.isActive? 'active' : 'inactive') ; }, 1000);
-
 
 /** 
  * This method create the new Donator_Object_Video that will be used to displayed
  *  information of the campaign on a lightbox
  */
-function createVideo(campaign, videoLink) {
-	return new Donator_Object_Video(campaign, videoLink);
+function createVideo(campaign) {
+    return new Donator_Object_Video(campaign);
 };
-	
-//});
--- wp-content/themes/modernize_v2-23/donator/widgets/DonatorVideo.js
+++ wp-content/themes/modernize_v2-23/donator/widgets/DonatorVideo.js
@@ -10,16 +10,24 @@
 * @author Sandra MGM
 */
 
-function Donator_Object_Video (video, videoLink) {
+function Donator_Object_Video (campaign) {
 	this._status = 'inactive';
+	this._analytics = "UA-38607035-1";
 	this._settings = {
-		src_webm: 'http://donator.es/wp-content/themes/modernize_v2-23/donator/videos/'+ video +'.webm',
-		src_mp4: 'http://donator.es/wp-content/themes/modernize_v2-23/donator/videos/'+ video +'.mp4',
-		placeholder: jQuery('.video-wrp'),
-		infoPlaceholder: jQuery('.video-info'),
-		contentPlaceholder: jQuery('.video-lightbox'),
-		videoLink: videoLink,
-		player:null
+		campaign:campaign,
+		//src_webm: 'http://donator.es/wp-content/themes/modernize_v2-23/donator/videos/'+ campaign +'.webm',
+		//src_mp4: 'http://donator.es/wp-content/themes/modernize_v2-23/donator/videos/'+ campaign +'.mp4',
+		
+		src_webm: 'http://stream.flowplayer.org/black/470x250.webm',
+		src_mp4: 'http://stream.flowplayer.org/black/470x250.mp4',
+		
+		placeholder: jQuery('#Donator_video'),
+		contentPlaceholder: jQuery('#Donator_video_lightbox'),
+		ongPlaceholder: jQuery('#Donator_ONGs'),
+		
+		videoLink: jQuery('.Donator_LightboxLink');,
+		player:null,
+		interval:null
 	};
 }
 
@@ -28,39 +36,85 @@ Donator_Object_Video.prototype = {
 	 * Contruct the html5 video element
 	 * */
 	videoHTML: function() {
+		var self = this;
+		
 		var html;
-		html =  '<video preload="none">' +
-				'	<source type="video/webm" src="'+ this._settings.src_webm +'"/>' +
-				'	<source type="video/mp4" src="'+ this._settings.src_mp4 +'"/>' +
-				'</video>';
+		html =  '<div class="video-wrp flowplayer main minimalist">' +
+			    '    <video preload="none">' +
+				'	    <source type="video/webm" src="'+ this._settings.src_webm +'"/>' +
+				'	    <source type="video/mp4" src="'+ this._settings.src_mp4 +'"/>' +
+				'    </video> ' +
+				'</div>';
 		
-		this._settings.placeholder.append(html);
+		this._settings.placeholder.html(html);
 		
-		jQuery(this._settings.placeholder).flowplayer({ 
-			seeking: false
+		jQuery('.video-wrp', this._settings.placeholder).flowplayer({
+			analytics: self._analytics,
+			splash: true,
+			keyboard: false
 		});
-		this._settings.player = flowplayer();
+		
+		this._settings.player = jQuery('.video-wrp', this._settings.placeholder).flowplayer();
+		
+		/* This code is needed to disable the seeking functionality */
+        jQuery(".fp-timeline").unbind("mousedown touchstart");
+        jQuery(".flowplayer").removeClass("is-touch");
+        
+        this._settings.player.bind("finish", function(){
+        	self.videoFinished();
+        });
 	},
 	
+	/**
+	 * Generates the html code for the list of ONGs available for the campaign
+	 * */
+	ongHTML: function() {
+		var self = this;
+		var html = '<li class="ong"><a href="">PALLAPUPAS</li>' +
+		    '<li class="ong">MÉDICOS SIN FRONTERAS</li>' +
+		    '<li class="ong">SPAM</li>'; 
+		
+		
+	},
+	
+	/**
+	 * Show video content on a lightbox
+	 * */
 	showLightbox: function() {
-		/**
-		 * Show video content on a lightbox
-		 * */
+		var self = this;
+		
+		this.videoHTML();
+		
 		this._settings.videoLink.fancybox({
 			maxWidth: document.width,
 			minWidth: document.width - 100,
 			maxHeight: document.height - 100,
 		       width: 700,
 		       height: 500,
-		    content: this.videoHTML(),
-			scrollOutside:true
+		    content: self._settings.contentPlaceholder,
+			scrollOutside:true,
+			afterClose : function() {
+		        self.reset(); 
+		        return;
+		    }
 		});
-		
-		/*jQuery('.fancybox-overlay').blur(function() {
-			  alert('Handler for .blur() called.');
-		});*/
-		//this.activeTab();
 	},
+	
+	videoFinished: function() {
+		if(this._settings.player.finished) {
+			console.log('VIDEO FINISHED');
+		}
+	},
+	
+	showSamePage: function() {
+		
+	},
+	
+	reset: function() {
+		if(this._settings) this._settings.placeholder.empty();
+		this._settings = null;
+		this._status = 'none';
+	}
 
 	
 };
